20c9e99a4bc794ae11ca7d50b985342cfdef2edf
Al Camargo <alcamargo@rocketmortgage.com>
2022-09-19 16:24:36 -0400

    initial commit

---
+++ Lab 8.1 PersonalWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_8._1_PersonalWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
