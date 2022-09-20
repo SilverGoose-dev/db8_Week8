adfd40120ef5b030951095574542427ce73e1496
Al Camargo <alcamargo@rocketmortgage.com>
2022-09-20 16:15:19 -0400

    EOD update

---
+++ Lab 8.2 Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Lab_8._2_Coffee_Shop_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab_8._2_Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult LoginLanding(string firstName, string lastName, string email, string password)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            ViewBag.Email = email;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab 8.2 Coffee Shop Registration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_8._2_Coffee_Shop_Registration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
